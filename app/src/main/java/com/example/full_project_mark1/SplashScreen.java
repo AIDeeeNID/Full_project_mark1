e6e181360cae09f1e16805e328a9540974c48c46
AIDeeeNID <113581307+AIDeeeNID@users.noreply.github.com>
2024-03-17 17:55:44 +0800

    Version 0.2

---
+++ app/src/main/java/com/example/full_project_mark1/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.full_project_mark1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+    }
+}
